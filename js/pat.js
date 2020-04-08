faefb31ac88d28e81f2f5f4c85290e008de4423e
derio123 <deriomatheus6@gmail.com>
2020-04-08 17:59:54 -0300

    Novo portfolio

---
+++ js/pat.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#Monitore').on('change', function() {
+        var selectVal = '#' + $(this).val();
+
+        $('#qtd1').children('div').hide();
+        $('#qtd1').children(selectVal).show();
+        $('#qtd2').children('div').hide();
+        $('#qtd2').children(selectVal).show();
+    })
+})
+
+$(document).ready(function($) {
+    $(window).resize(function(event) {
+        $('body').css({ marginTop: $('.navbar').height() + 'px' });
+    }).trigger('resize');
+});
